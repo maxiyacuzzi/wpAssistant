a36544e0c1b665e996db2e861e699bbd031a95a6
maxiyacuzzi <maxiyacuzzi@hotmail.com>
2025-03-16 00:18:52 -0300

    estructura basica

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"whatsapp-ai-assistant/config"
+	"whatsapp-ai-assistant/routes"
+)
+
+func main() {
+	// Cargar variables de entorno
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error cargando el archivo .env")
+	}
+
+	// Conectar a la base de datos
+	config.ConnectDB()
+
+	// Inicializar servidor
+	r := gin.Default()
+
+	// Definir rutas
+	routes.SetupRoutes(r)
+
+	// Obtener el puerto de las variables de entorno
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	// Iniciar el servidor
+	log.Println("Servidor corriendo en el puerto " + port)
+	r.Run(":" + port)
+}
